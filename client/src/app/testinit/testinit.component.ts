bedb63cf6a0d059f3dec502e2c5fc22b4f1b7810
Thominik <dominikbednarz.ti@gmail.com>
2022-03-17 21:58:50 +0100

    Init

---
+++ client/src/app/testinit/testinit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testinit',
+  templateUrl: './testinit.component.html',
+  styleUrls: ['./testinit.component.css']
+})
+export class TestinitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
