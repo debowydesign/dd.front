1311b94bd2fe60ed890f50e1141e8f2dc27befb6
Thominik <dominikbednarz.ti@gmail.com>
2022-04-20 23:09:18 +0200

    RouterLinks

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
